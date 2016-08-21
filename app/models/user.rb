22736479af1df841f8fa62d05ef35e3b936894d2
Georgios Lymperopoulos <glympe@yahoo.gr>
2016-08-21 13:03:08 +0300

    Add admin user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :articles
+  has_many :articles, dependent: :destroy
   # make email before added to db, downcase
   before_save { self.email = email.downcase }
 
